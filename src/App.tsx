c44a047505f4fb10b408355b26d74c0ebb76d08d
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-12-17 20:24:01 +0100

    🚧 feat: Creation festival: name + date start/end

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { StyledEngineProvider } from '@mui/material/styles';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import 'dayjs/locale/fr';
 
 import "./styles/styles.scss"
 import variables from "./styles/abstract/variables.module.scss"
 import HomePage from './pages/HomePage/HomePage';
+import CreateFestival from './pages/CreateFestival/CreateFestival';
 
 
 let theme = createTheme({
@@ -108,16 +112,19 @@ theme = responsiveFontSizes(theme);
 const App = () => {
 
   return (
-    <ThemeProvider theme={theme}>
-      <StyledEngineProvider injectFirst>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="*" element={<Navigate replace to="/" />} />
-          </Routes>
-        </BrowserRouter>
-      </StyledEngineProvider>
-    </ThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fr">
+        <ThemeProvider theme={theme}>
+        <StyledEngineProvider injectFirst>
+            <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/festival/creation" element={<CreateFestival />}/>
+                <Route path="*" element={<Navigate replace to="/" />} />
+            </Routes>
+            </BrowserRouter>
+        </StyledEngineProvider>
+        </ThemeProvider>
+    </LocalizationProvider>
   );
 }
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from "@mui/material"
-import "../../styles/pages/homepage.scss" 
 
 const HomePage = () => {
 	return(
