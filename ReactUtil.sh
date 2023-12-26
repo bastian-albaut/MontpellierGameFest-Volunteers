ae0a34a751ca50888d6e9e6a48f81ba2bed1d5bd
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-12-26 16:52:09 +0100

    🔨 ci: Improve script to create components

--- ReactUtil.sh
+++ ReactUtil.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-command=$1
-arg=$2
-sass=$3
-name=$4
+read -p "Enter the type of file you want to create (\"component\" or \"page\"): " arg
+
+if [ "$arg" != "component" ] && [ "$arg" != "page" ]; then
+  echo "Invalid type. Exiting..."
+  exit 1
+fi
+
+read -p "Do you want to create a sass file? (0 for no, 1 for yes): " sass
+
+if [ "$sass" != 0 ] && [ "$sass" != 1 ]; then
+  echo "Invalid choice for Sass. Exiting..."
+  exit 1
+fi
+
+read -p "Enter new file name: " name
+read -p "Enter new directory name: " directoryName
 
 create_folder_file () {
-	mkdir -p src/$arg\s/$name
-	touch src/$arg\s/$name/$name.jsx
+    mkdir -p src/$arg\s/$directoryName
+    touch src/$arg\s/$directoryName/$name.tsx
 }
 
 create_with_sass () {
 	create_folder_file
 	(
-	echo "import \"../../styles/${arg}s/$(tr [A-Z] [a-z] <<< "$name").scss\" "
+	echo "import \"../../styles/${arg}s/$directoryName/$(tr [A-Z] [a-z] <<< "$name").module.scss\" "
 	echo ""
 	echo "const $name = () => {"
 	echo -e "\treturn("
@@ -22,10 +34,10 @@ create_with_sass () {
 	echo "}"
 	echo ""
 	echo "export default $name"
-	) > src/$arg\s/$name/$name.jsx
+	) > src/$arg\s/$directoryName/$name.tsx
 
-	mkdir -p src/styles/$arg\s
-	touch src/styles/$arg\s/"$(tr [A-Z] [a-z] <<< "$name")".scss
+	mkdir -p src/styles/$arg\s/$directoryName
+	touch src/styles/$arg\s/$directoryName/"$(tr [A-Z] [a-z] <<< "$name")".module.scss
 }
 
 create_without_sass () {
@@ -39,57 +51,43 @@ create_without_sass () {
 	echo "}"
 	echo ""
 	echo "export default $name"
-	) > src/$arg\s/$name/$name.jsx
+	) > src/$arg\s/$directoryName/$name.tsx
 }
 
-if [ $command == "create" ]
+if [ "$arg" == "component" ]
+then
+    
+    if [ "$sass" == 1 ]
+    then
+        create_with_sass
+        echo "You successfully created component ${name} with sass in directory ${directoryName}"
+    elif [ "$sass" == 0 ]
+    then
+        create_without_sass
+        echo "You successfully created component ${name} without sass in directory ${directoryName}"
+    else
+        echo "Invalid choice for Sass. Exiting..."
+        exit 1
+    fi
+
+elif [ "$arg" == "page" ] 
 then
 
-	if [ $arg == "component" ]
-	then
-		
-		if [ $sass == 1 ]
-		then
-  			create_with_sass
-  			echo "You successfully created component ${name} with sass"
-		elif [ $sass == 0 ]
-		then
-  			create_without_sass
-  			echo "You successfully created component ${name} without sass"
-		else
-  			echo "wrong third arg"
-  			exit 1
-		fi
-
-	elif [ $arg == "page" ] 
-	then
-
-		if [ $sass == 1 ]
-                then
-                        create_with_sass
-                        echo "You successfully created page ${name} with sass"
-		elif [ $sass == 0 ]
-                then
-                        create_without_sass
-                        echo "You successfully created page ${name} without sass"
-                else
-                        echo "wrong third arg"
-                        exit 1
-                fi
-
-
-	else
- 		echo "invalid second arg"
- 		exit 1
-	fi
-elif [ $command == "help" ]
-then
-	echo "./ReactUtil.sh create <component|page> <sass?> <name>"
-	echo "./ReactUtil.sh help"
+    if [ "$sass" == 1 ]
+    then
+        create_with_sass
+        echo "You successfully created page ${name} with sass in directory ${directoryName}"
+    elif [ "$sass" == 0 ]
+    then
+        create_without_sass
+        echo "You successfully created page ${name} without sass in directory ${directoryName}"
+    else
+        echo "Invalid choice for Sass. Exiting..."
+        exit 1
+    fi
 
 else
-	echo "invalid command"
-	exit 1
+    echo "Invalid type. Exiting..."
+    exit 1
 fi
-
 exit 0
