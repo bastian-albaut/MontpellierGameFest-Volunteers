9d8dae8223d75daef022f552d2263addd381db2e
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-12-26 17:08:06 +0100

    ✨ feat: Add toolbar for connected and disconnected user

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
 import { Box, Typography } from "@mui/material"
 import "../../styles/pages/homepage.scss" 
+import Appbar from "../../components/general/Appbar"
 
 const HomePage = () => {
 	return(
-    <Box className="boxHomepage">
-      <Typography variant="h1" color="initial" id="title">Page d'accueil</Typography>
-    </Box>
+        <>
+            <Appbar />
+            <Box className="boxHomepage">
+                <Typography variant="h1" color="initial" id="title">Page d'accueil</Typography>
+            </Box>
+        </>
 	)
 }
 
