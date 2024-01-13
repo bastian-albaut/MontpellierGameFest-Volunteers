3cbd696f3e89611b618f880984ff723d23a82aae
Alexandre Lagorce <127868503+alexlagorce@users.noreply.github.com>
2024-01-13 19:02:34 +0100

    listefestivals

--- src/components/Dashboard/SectionDashboard.tsx
+++ src/components/Dashboard/SectionDashboard.tsx
@@ -1,7 +1,7 @@
 import "../../styles/components/Dashboard/sectiondashboard.module.scss" 
 import React, { useState, useEffect } from 'react';
 import { Typography, List, ListItem, ListItemText, Button, Box } from '@mui/material';
-
+import { getFestivals } from '../../api';
 
 interface Festival {
 	id: string;
@@ -12,27 +12,67 @@ interface Festival {
 }
 
 const SectionDashboard: React.FC = () => {
-    const [festivals, setFestivals] = useState<Festival[]>([]);
+
+	// Exemple de données de festivals pour le test
+    const exampleFestivals: Festival[] = [
+        {
+            id: '1',
+            nom: 'Festival du Printemps',
+            lieu: 'Montpellier',
+            dateDebut: '2023-04-10',
+            dateFin: '2023-04-15'
+        },
+        {
+            id: '2',
+            nom: 'Festival de Musique Électronique',
+            lieu: 'Paris',
+            dateDebut: '2023-07-20',
+            dateFin: '2023-07-22'
+        },
+        {
+            id: '3',
+            nom: 'Festival de Jazz',
+            lieu: 'Lyon',
+            dateDebut: '2023-08-05',
+            dateFin: '2023-08-10'
+        }
+    ];
+
+	const [festivals, setFestivals] = useState<Festival[]>(exampleFestivals);
+
+    //const [festivals, setFestivals] = useState<Festival[]>([]);
 
 
-	useEffect(() => { 
-		fetch("http://localhost:3000/api/festivals")
-			.then((res) => res.json())
-			.then((data) => {
-				setFestivals(data);
-			});
-	},[]);
+
+	const fetchFestivals = async () => {
+		try {
+			const res = await getFestivals();
+			console.log("Response from getFestivals:", res);
+			if (res.data) {
+				setFestivals(res.data);
+			}
+		} catch (error) {
+			console.error('Error fetching festivals data', error);
+		}
+	}
+
+
+	useEffect(() => {
+		// A tester avec la base de donnée
+		//fetchFestivals();
+	}, []);
 
 
 	return (
         <Box sx={{ margin: 3 }}>
             <Typography variant="h2" gutterBottom>
-                Liste des Festivals
+                Liste des Festivals ouverts à l'inscription
             </Typography>
             <List>
                 {festivals.map((festival, index) => (
                     <ListItem key={index} divider>
                         <ListItemText primary={festival.nom} />
+						// TODO: Ajouter un bouton pour s'inscrire au festival
                         <Button variant="contained" color="primary">
                             S'inscrire
                         </Button>
