876562aecd2e9cb722b88eeef60c065e808104b1
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2024-01-14 14:45:49 +0100

    🔒️ fix: Redirect to homepage on dashboard and creation festival if not logged in

--- src/pages/CreateFestival/CreateFestival.tsx
+++ src/pages/CreateFestival/CreateFestival.tsx
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import SectionCreateFestival from "../../components/CreateFestival/SectionCreateFestival"
 import Appbar from "../../components/general/Appbar";
 import Loading from "../../components/general/Loading";
 import { useUser } from "../../contexts/UserContext";
+import { useEffect } from "react";
 
 const CreateFestival = () => {
 
     // Get the current user
     const { user, loading } = useUser();
+
+    // Redirect to home page if not logged in
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!user && !loading) {
+            navigate("/");
+        }
+    }, [user, loading, navigate]);
 
     if (loading) {
         return <Loading />;
