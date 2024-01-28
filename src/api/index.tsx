675495a72a8452087cada1da206deb34ed6b041b
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2024-01-28 12:14:55 +0100

    🐛 fix: Erratum route creation festival

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -16,7 +16,7 @@ API.interceptors.request.use((req) => {
 export const login = (data: any) => API.post('/login', data);
 export const register = (data: User) => API.post('/register', data);
 export const getCurrentUser = () => API.get('/users/current');
-export const createFestival = (data: any) => API.post('/festival', data);
+export const createFestival = (data: any) => API.post('/festivals', data);
 export const getPosteById = (idPoste: string) => API.get(`/postes/${idPoste}`);
 export const getFestivalById = (id: string) => API.get(`/festivals/${id}`);
 export const getPostesByFestival = (id: string) => API.get(`/festivals/${id}/postes`);
