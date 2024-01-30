79724a0aa9f90c5336588192c840bf4916121983
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2024-01-30 23:19:28 +0100

    🐛 fix: Same bug with UTC in signupFestival

--- src/components/SignupFestival/SectionSignupFestival.tsx
+++ src/components/SignupFestival/SectionSignupFestival.tsx
@@ -110,8 +110,8 @@ const SectionSignupFestival = () => {
 
     // Format time
     function formatTimeToHHMM(date: Date) {
-        const hours = date.getHours().toString().padStart(2, '0');
-        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const hours = date.getUTCHours().toString().padStart(2, '0');
+        const minutes = date.getUTCMinutes().toString().padStart(2, '0');
         return `${hours}:${minutes}`;
       }
 
