437fb411c3bcd4c3d0f79a2d23e332c3cb22d1b2
Lucas Leroy <lucas.leroy02@etu.umontpellier.fr>
2024-02-03 23:27:44 +0100

    ajout my fest maj

--- src/components/Dashboard/Planning.tsx
+++ src/components/Dashboard/Planning.tsx
@@ -1,25 +1,32 @@
 import styles from '../../styles/components/Dashboard/dashboard.module.scss';
+
 import React, { useEffect, useState } from 'react';
 import { getCreneauxByFestival } from '../../api'; // Ajustez selon votre API
 import { DataGrid } from '@mui/x-data-grid';
 import Loading from '../general/Loading'; // Vérifiez le chemin
 import { Box, Typography } from '@mui/material';
+import { Creneau } from '../../types/Creneau';
 
 
 
-interface PlanningProps {
-  idFestival: string;
+interface PlanningProps  {
+  idFestival: string ;
 }
 
 const Planning: React.FC<PlanningProps> = ({ idFestival }) => {
-  const [creneaux, setCreneaux] = useState<any[]>([]); // Type spécifique si possible
+  const [creneaux, setCreneaux] = useState<Creneau[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCreneaux = async () => {
       try {
         const response = await getCreneauxByFestival(idFestival);
-        setCreneaux(response.data); // Adaptez le format de la réponse
+        // Typage explicite de la variable 'creneau' dans le callback map
+        const transformedData = response.data.map((creneau: Creneau) => ({
+          ...creneau,
+          id: creneau.idCreneau, // Utilisez l'ID de créneau comme clé unique pour chaque ligne
+        }));
+        setCreneaux(transformedData);
       } catch (error) {
         console.error('Erreur lors de la récupération des créneaux', error);
       } finally {
@@ -46,14 +53,18 @@ const Planning: React.FC<PlanningProps> = ({ idFestival }) => {
   ];
 
   return (
+    <>
+
     <Box sx={{ height: 400, width: '100%' }}>
       <Typography variant="h5" className={styles.heading}>Planning du Festival</Typography>
       <DataGrid
         rows={creneaux}
         columns={columns}
+        getRowId={(row) => row.idCreneau}
       />
     </Box>
-  );
+    
+</> );
 };
 
 export default Planning;
---
+++ src/types/Creneau.ts
@@ -0,0 +1,8 @@
+export interface Creneau {
+    idCreneau: number;
+    timeStart: Date;
+    timeEnd: Date;
+    idFestival: number;
+    creneauEspace: any[]; // Remplacez any par un type approprié si vous en avez un
+    // Ajoutez toutes les autres propriétés nécessaires ici
+  }
