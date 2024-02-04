05aab0ce4782135f29b3e69bebea7e5c09140778
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2024-02-04 17:41:07 +0100

    🚑 fix: Create creneau for each day in creation festival

--- src/components/CreateFestival/SectionCreateFestival.tsx
+++ src/components/CreateFestival/SectionCreateFestival.tsx
@@ -296,17 +296,30 @@ const SectionCreateFestival = () => {
     // Create all the creneaux for the festival
     const handleCreneauxCreation = async (idFestival: number) => {
         try {
-            const creneauxToCreate : Creneau[] = dataCreneau.map((creneau: any) => {
-                const { id, timeStart, timeEnd, ...newCreneau } = creneau;
+            const dates: Date[] = getListDatesBetweenDates(dataFestival.dateDebut.toDate(), dataFestival.dateFin.toDate());
 
-                // Convert time strings to Date objects
-                const formattedTimeStart = parseTimeStringToDate(timeStart);
-                const formattedTimeEnd = parseTimeStringToDate(timeEnd);
+            const creneauxToCreate: Creneau[] = [];
 
-                return { ...newCreneau, idFestival, timeStart: formattedTimeStart, timeEnd: formattedTimeEnd };
-            });
+            for (const date of dates) {
+                for (const creneau of dataCreneau) {
+                    const { id, timeStart, timeEnd } = creneau;
+                    
+                    // Convert time strings to Date objects
+                    const formattedTimeStart = parseTimeStringToDate(timeStart, date);
+                    const formattedTimeEnd = parseTimeStringToDate(timeEnd, date);
+
+                    const creneauToAdd: Creneau = {
+                        idFestival,
+                        timeStart: formattedTimeStart,
+                        timeEnd: formattedTimeEnd,
+                    };
+
+                    creneauxToCreate.push(creneauToAdd);
+                }
+            }
+
             const res = await addMultipleCreneau(creneauxToCreate);
-            if(res && res.data) {
+            if (res && res.data) {
                 return true;
             } else {
                 return false;
@@ -317,13 +330,24 @@ const SectionCreateFestival = () => {
         }
     }
     
-    const parseTimeStringToDate = (timeString: string) => {
+    const parseTimeStringToDate = (timeString: string, date: Date) => {
         const [hours, minutes] = timeString.split(':');
-        const currentDate = new Date();
-        currentDate.setUTCHours(parseInt(hours, 10));
-        currentDate.setUTCMinutes(parseInt(minutes, 10));
-        return currentDate;
+        const formattedDate = new Date(date);
+        formattedDate.setUTCHours(parseInt(hours));
+        formattedDate.setUTCMinutes(parseInt(minutes));
+        return formattedDate;
     };
+
+    const getListDatesBetweenDates = (startDate: Date, endDate: Date) => {
+        const dates = [] as Array<Date>;
+        let currentDate = startDate;
+        while (currentDate <= endDate) {
+            dates.push(new Date(currentDate));
+            currentDate = new Date(currentDate);
+            currentDate.setDate(currentDate.getDate() + 1);
+        }
+        return dates;
+    }
 
     // Create the espace default for each poste of the festival
     const handleEspaceCreation = async (idFestival : number, listIdEspaceCreated: Array<number>) => {
